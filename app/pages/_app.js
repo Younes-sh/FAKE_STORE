fa07a7326482087a8f765e4f87979f9c23c95f29
younes-sh <sheikhlaryounes@gmail.com>
2025-01-18 20:33:16 +0100

    I edited pages/_app.js

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,5 +1,13 @@
 import "@/styles/globals.css";
+import Navbar from "@/Components/Navbar/Navbar";
+import Footer from "@/Components/Footer";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
