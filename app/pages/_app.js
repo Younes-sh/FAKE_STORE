2b6f667e150e7e162080993832627184004a6768
younes-sh <sheikhlaryounes@gmail.com>
2025-01-19 17:37:26 +0100

    I Edited pages/_app.js

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,11 +1,26 @@
 import "@/styles/globals.css";
-import Navbar from "@/Components/Navbar/Navbar";
+import NavbarBeforLogin from "@/Components/Navbar/NavbarBeforLogin";
+import NavbarAfterLogin from "@/Components/Navbar/NavbarAfterLogin";
 import Footer from "@/Components/Footer";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }) {
+  const [isNavbar, setIsNavbar] = useState(false);
+  useEffect(() => {
+    const token = window.localStorage.getItem('token');
+    if (!token) {
+      setIsNavbar(false);
+    } else {
+      setIsNavbar(true);
+    }
+  })
   return (
     <>
-      <Navbar />
+      {isNavbar? (
+        <NavbarAfterLogin />
+      ) : (
+        <NavbarBeforLogin />
+      )}
       <Component {...pageProps} />
       <Footer />
     </>
