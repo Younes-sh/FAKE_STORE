853c22b1bf3b9d5200df57bc2a7c6bec2747c395
younes-sh <sheikhlaryounes@gmail.com>
2025-05-06 18:19:01 +0200

    I implemented responsiveness for the Basket page.

--- app/pages/basket/index.js
+++ app/pages/basket/index.js
@@ -1,7 +1,45 @@
+import Style from './basket.module.css';
+import { useContext, useMemo  } from 'react';
+import { AppContext } from '../_app';
+import BasketCard from '../../Components/Cards/BasketCard/BasketCard';
+import Link from 'next/link';
+
 export default function BasketPage () {
+    const {addToCard, addProduct} = useContext(AppContext);
+
+    // استفاده از useMemo برای محاسبه مجموع قیمت به صورت کارآمد
+    const totalPaye = useMemo(() => {
+        return addProduct.reduce((sum, item) => {
+            // اطمینان حاصل کنید که item.totalPrice وجود دارد و یک عدد است
+            const price = typeof item.totalPrice === 'number' ? item.totalPrice : 0;
+            return sum + price;
+        }, 0);
+    }, [addProduct]); // وابستگی به addProduct تا در صورت تغییر آن، دوباره محاسبه شود
+
     return (
-        <div className="container main">
-            <h1>Basket Page</h1>
+        <div >
+                <div className="container main">
+                    <div className={Style.Subtotal}>
+                        <h3>Subtotal</h3>
+                        <p>Total pay: $ {totalPaye}</p>
+                        <div className={Style.checkoutBtn}>
+                            <Link href={''}>purchase to checkout</Link>
+                        </div>
+                    </div>
+                    <div className={Style.basket}>
+
+                        {addToCard > 0 ? (
+                            <div>
+                                {
+                                  addProduct.map(item => <BasketCard key={item._id} {...item} />)
+                                }
+                            </div>) : (<div><p>Your basket is empty</p></div>)
+                        }
+                    </div>
+                </div>
+            
+
         </div>
     )
 }
+
