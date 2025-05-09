6749010833557ce2c262baef795dfa66789c7054
younes-sh <sheikhlaryounes@gmail.com>
2025-05-09 19:42:28 +0200

    next.config.mjs

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'younessheikhlar.com',
+        // اگر نیاز به محدود کردن پورت یا مسیر خاصی دارید، می‌توانید از port و pathname استفاده کنید.
+        // port: '',
+        // pathname: '/images/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+// https://younessheikhlar.com/
