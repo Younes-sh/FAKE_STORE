ba695e73ff7fd1b389a93fee2e7076f3b8f80068
younes-sh <sheikhlaryounes@gmail.com>
2025-05-09 19:33:36 +0200

    Components/Footer/index.js

--- app/Components/Footer/index.js
+++ app/Components/Footer/index.js
@@ -3,9 +3,7 @@ import Style from './footer.module.css';
 export default function Footer () {
     return (
         <div className={Style.footer}>
-            <footer>
-                &copy; 2023 Fake Store. All rights reserved.
-            </footer>
+            <p>&copy; 2023 Fake Store. All rights reserved.</p>
         </div>
     )
 }
