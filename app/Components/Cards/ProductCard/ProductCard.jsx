68d95a545a83bce8929306ed6ba55fc108636094
younes-sh <sheikhlaryounes@gmail.com>
2025-05-09 19:29:12 +0200

    I added button to buy

--- app/Components/Cards/ProductCard/ProductCard.jsx
+++ app/Components/Cards/ProductCard/ProductCard.jsx
@@ -1,22 +1,68 @@
 import Style from "./style.module.css";
-import React from 'react'
-import ImageTest from "@/public/backgroundWatch.jpg";
+import React, { useContext } from 'react';
 import Image from "next/image";
-export default function ProductCard() {
+import Link from "next/link";
+import { AppContext } from '../../../pages/_app';
+
+export default function ProductCard({
+  _id, productName, description, price, model, section, image, count
+}) {
+  const {
+    addToCard,
+    setAddToCard,
+    addProduct,
+    setAddProduct
+  } = useContext(AppContext);
+
+  const addProductBtn = () => {
+    
+    if(isExisteProduct()){
+      const UpdateProduct = [...addProduct]
+      UpdateProduct.map(product => {
+        if(product._id === _id) {
+          product.count += 1;
+          product.totalPrice = product.price * product.count
+        }
+        setAddProduct(UpdateProduct);
+        return 
+      })
+    }else {
+        setAddToCard(addToCard + 1);
+        const newProduct = {
+          _id,
+          productName,
+          price,
+          count: 1, // مقدار اولیه تعداد
+          totalPrice: price * 1, // مقدار اولیه قیمت کل
+          image
+          // ... سایر پراپرتی های مورد نیاز
+        };
+        setAddProduct( prevent => [...prevent, newProduct]);
+      }
+    
+  };
+
+  function isExisteProduct () {
+    return addProduct.some(product => product._id === _id);
+  }
+
   return (
     <div className={Style.productCard}>
+      <Link href={`/products/${_id}`} className={Style.Link}>
         <div className={Style.imageContainer}>
-          <Image src={ImageTest} alt="Product" />
+          <Image src={image} alt={productName} width={16} height={9} layout="responsive" objectFit="cover" />
         </div>
-        <div className={Style.TextContainer}>
-          <h3>Product Name</h3>
-          <p>Product Description</p>
-          <p>Price: $9.99</p>
-          <div className={Style.buttonContainer}>
-            <button className={Style.btnAddToCard}>Add to Cart</button>
-            <button className={Style.btnBuy}>Buy Now</button>
-          </div>
+      </Link>
+      <div className={Style.TextContainer}>
+        <h4>{productName}</h4>
+        <p>Price: {price}</p>
+        <div className={Style.buttonContainer}>
+          <button className={Style.btnAddToCard} onClick={addProductBtn}>
+            Add to Cart
+          </button>
+          <button className={Style.btnBuy}>Buy Now</button>
         </div>
+      </div>
     </div>
-  )
+  );
 }
