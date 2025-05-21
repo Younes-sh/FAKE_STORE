9d7ddbe8752c9e317577fcf92f74acc028f3d8c6
younes-sh <sheikhlaryounes@gmail.com>
2025-05-21 17:31:51 +0200

    I created pages/products/index.js

--- app/pages/products/index.js
+++ app/pages/products/index.js
@@ -1,27 +1,31 @@
 import React from 'react';
-import ProductCard from '@/Components/Cards/ProductCard/ProductCard';
+// import ProductCard from '@/Components/Cards/ProductCard/ProductCard';
+import ProductCard from '@/Components/Cards/CardProductTest/CardProductTest';
 
-export default function index() {
+import Style from "./style.module.css";
+
+export default function index({ productData }) {
   return (
-    <div className='container main'>
-      <h1>Products</h1>
-      <div className='row'>
-        <div className='col'>
-          <ProductCard />
-        </div>
-
-        <div className='col'>
-          <ProductCard />
-        </div>
-
-        <div className='col'>
-          <ProductCard />
-        </div>
-
-        <div className='col'>
-          <ProductCard />
+    <div className={Style.backgroundProduct}>
+      <div className='container'>
+        <h1>Products</h1>
+        <div className='row'>
+            {productData.map(product => (
+              <div className='col' key={product._id}>
+                <ProductCard  {...product}/>
+              </div>
+            ))}
         </div>
       </div>
     </div>
   )
+}
+
+
+export async function getServerSideProps() {
+  const res = await fetch('http://localhost:3000/api/products');
+  const data = await res.json();
+  return {
+    props: { productData: data.products}
+  }
 }
