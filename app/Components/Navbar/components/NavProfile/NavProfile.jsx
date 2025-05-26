b36e97c7f93c9b7f3cd0c05e4bbf33ed1578225b
younes-sh <sheikhlaryounes@gmail.com>
2025-05-26 19:22:27 +0200

    I remoed a Link for Icon profile!

--- app/Components/Navbar/components/NavProfile/NavProfile.jsx
+++ app/Components/Navbar/components/NavProfile/NavProfile.jsx
@@ -19,11 +19,9 @@ export default function NavProfile() {
   }
   return (
     <div>
-      <div onClick={OpenMenuProfile}>
+      <div className={Style.OpenMenuProfile} onClick={OpenMenuProfile}>
         {/* <Image src={User} alt="Image" width={30} height={30} /> */}
-        <Link href={''}>
           <i className="fa-solid fa-user"></i>
-        </Link>
       </div>
 
 
