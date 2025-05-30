e26efe655cea398f3e875e4348b3f71620e83441
younes-sh <sheikhlaryounes@gmail.com>
2025-05-30 18:17:48 +0200

    I created pages/younessheikhlar/products/index.jsx

--- app/pages/younessheikhlar/products/index.jsx
+++ app/pages/younessheikhlar/products/index.jsx
@@ -1,17 +1,32 @@
-import React, { useState } from 'react';
-import AdminLayout from '@/Components/AdminLayout/AdminLayout';
+import React, { useState , useEffect} from 'react';
+import AdminLayout from '@/Components/Admin/AdminLayout/Layout';
 import ProductCard from '@/Components/Admin/Cards/ProductCard/ProductCard';
+import InputSearch from '@/Components/Input/Input';
 
 export default  function index({ productData }) {
+  const [textSearch, setTextSearch] = useState('');
+  const [filteredProducts, setFilteredProducts] = useState(productData);
+
+  // useEffect(() => {
+  //   const filtered = productData.filter(product =>
+  //     product.name.toLowerCase().includes(textSearch.toLowerCase())
+  //   );
+  //   setFilteredProducts(filtered);
+  // }, [textSearch, productData]);
+
   
   return (
     <AdminLayout>
       <h1>Products</h1>
-      
+      <br/>
+
+      {/* Input search, this input comes from (component input) */}
+      <InputSearch onChange={(e) => setTextSearch(e.target.value)}  />
+
       <div className='row'>
           {productData.map(product => (
-        <div className='col'>
-            <ProductCard key={product._id} {...product}/>
+        <div className='col' key={product._id}>
+          <ProductCard  {...product}/>
         </div>
           ))}
       </div>
