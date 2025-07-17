9986507f50d5ab62a9ceec1443010f66e201ef71
younes-sh <sheikhlaryounes@gmail.com>
2025-07-17 20:29:04 +0200

    I solved the problem with Link in singleItem page for getServerSideProps

--- app/pages/products/[singleItem].js
+++ app/pages/products/[singleItem].js
@@ -87,7 +87,7 @@ export default function SingleItem({ dataProduct }) {
 const baseURL = process.env.NEXTAUTH_URL;
 export async function getServerSideProps(context) {
   const { singleItem } = context.params;
-  const res = await fetch(`${baseURL}/api/products/${singleItem}`);
+  const res = await fetch(`${process.env.URL}/api/products/${singleItem}`);
   const data = await res.json();
   return {
     props: { dataProduct: data.data },
