900828e472775d20764eb25e885da18e685bb571
younes-sh <sheikhlaryounes@gmail.com>
2025-08-04 18:26:53 +0200

    I edited the NavProfile: I added the Link of Admin page, when session is equal to (admin), Admin link appears

--- app/Components/Navbar/components/NavProfile/NavProfile.jsx
+++ app/Components/Navbar/components/NavProfile/NavProfile.jsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import User from "@/public/user.png";
 import Image from "next/image";
 import Style from './navProfile.module.css';
-import { signOut } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 
 export default function NavProfile() {
-  const [isOpen , setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const { data: session, status } = useSession();
 
   const OpenMenuProfile = () => {
     setIsOpen(!isOpen);
@@ -15,24 +16,17 @@ export default function NavProfile() {
     setIsOpen(false);
   }
 
-  
   return (
     <div>
       <div className={Style.OpenMenuProfile} onClick={OpenMenuProfile}>
-        {/* <Image src={User} alt="Image" width={30} height={30} /> */}
-          <i className="fa-solid fa-user"></i>
+        <i className="fa-solid fa-user"></i>
       </div>
-
 
       {/* Menu Profile */}
       {isOpen && (
         <div className={Style.menuProfile} onClick={CloseMenuProfile}>
           <p>
             <Link href="/profile">Profile</Link>
-          </p>
-
-          <p>
-            <Link href="/profile">Orders</Link>
           </p>
 
           <p>
@@ -43,12 +37,18 @@ export default function NavProfile() {
             <Link href="/notification">Notification</Link>
           </p>
 
+          {session?.user?.role === "admin" && (
+            <p>
+              <Link href="/younessheikhlar">Admin</Link>
+            </p>
+          )}
+
           <br />
           <br />
           <br />
 
-            <button type="button" onClick={() => signOut()}>Logout</button>
-      </div>
+          <button type="button" onClick={() => signOut()}>Logout</button>
+        </div>
       )}
     </div>
   )
