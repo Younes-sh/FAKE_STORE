d232c210b80de00d8d2973c20e9d6c52f3ab1a3a
younes-sh <sheikhlaryounes@gmail.com>
2025-08-31 15:28:44 +0200

    I removed envirement PUBLIC_NEXT_PUBLIC_APP_URL

--- app/pages/verify/index.js
+++ app/pages/verify/index.js
@@ -24,7 +24,7 @@ export default function VerifyPage() {
       const data = await res.json();
       if (!res.ok) throw new Error(data.message || "Failed");
       setMsg("Email verified! You can now log in.");
-      router.push(`${process.env.NEXT_PUBLIC_APP_URL}/login`)
+      router.push(`/login`)
     } catch (err) {
       setMsg(err.message || "Error");
     } finally {
