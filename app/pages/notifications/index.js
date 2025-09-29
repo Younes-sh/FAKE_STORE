cac4bda8f36382371e86f444c24fba2cb50ac4e4
younes-sh <sheikhlaryounes@gmail.com>
2025-09-29 19:42:44 +0200

    I changed name of Notification page

--- app/pages/notifications/index.js
+++ app/pages/notifications/index.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { useSession } from 'next-auth/react';
 import Style from './Notifications.module.css';
 
-export default function NotificationsPage() {
+export default function Notifications() {
   const { data: session, status } = useSession({ required: true });
   const [notifications, setNotifications] = useState([]);
   const [loading, setLoading] = useState(true);
